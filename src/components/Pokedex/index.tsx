aa51c6d47309cdde327d94d6c1731951296f145f
Seu nome <yago_bohmcruz@hotmail.com>
2024-08-28 14:24:17 -0300

    I DONT KNOW MORE

--- src/components/Pokedex/index.tsx
+++ src/components/Pokedex/index.tsx
@@ -1,17 +1,55 @@
 import { IconSearch } from "@tabler/icons-react";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import PokemonList from "../PokemonList";
+import { useIntersectionObserver } from "../../hooks/intersectionObserver";
+
+interface Pokemon {
+  name: string;
+  id: number;
+  img: string;
+}
+
+const INITIAL_LIMIT = 898;
+const INCREASE_LIMIT = 20;
 
 const Pokedex: React.FC = () => {
-  const [allPokemons, setAllPokemons] = React.useState([]);
+  const [allPokemons, setAllPokemons] = React.useState<Pokemon[]>([]);
+  const [pokemonName, setPokemonName] = React.useState("");
+
+  const [limit, setLimit] = React.useState(INITIAL_LIMIT);
+
+  const targetObserver = useRef<HTMLSpanElement>(null);
+  const entry = useIntersectionObserver(targetObserver, {});
+  const isVisible = !!entry?.isIntersecting;
+
+  const pokemonsByName = allPokemons.filter((pokemon) =>
+    pokemon.name.includes(pokemonName),
+  );
+
+  const handleChangePokemonName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPokemonName(e.target.value.toLowerCase());
+  };
 
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=898")
       .then(({ data }) => setAllPokemons(data.results))
       .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      const maxPokemons = pokemonsByName.length;
+      const newLimit = limit + INCREASE_LIMIT;
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      newLimit > maxPokemons ? setLimit(maxPokemons) : setLimit(newLimit);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    setLimit(INITIAL_LIMIT);
+  }, [pokemonName]);
 
   return (
     <section className='p-4 py-5'>
@@ -21,13 +59,18 @@ const Pokedex: React.FC = () => {
             className='outline-none flex-1'
             type='text'
             placeholder='Search your Pokemon'
+            name='pokemonName'
+            autoComplete='off'
+            onChange={handleChangePokemonName}
           />
           <button className='bg-indigo-700 p-2 rounded-xl hover:bg-indigo-500 transition-colors '>
             <IconSearch color='white' stroke={2} />
           </button>
         </div>
       </form>
-      <PokemonList pokemons={allPokemons} />
+      <PokemonList pokemons={pokemonsByName.slice(0, limit)} />
+      {/* Target Observer */}
+      <span ref={targetObserver}></span>
     </section>
   );
 };
--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -3,14 +3,14 @@ import PokemonPreview from "../PokemonPreview";
 
 interface Pokemon {
   name: string;
-  url: string;
+  url?: string;
 }
 
 const PokemonList: React.FC<{ pokemons: Pokemon[] }> = ({ pokemons }) => {
   return (
     <section className='pt-12 grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-4 gap-y-10'>
       {pokemons.map((pokemon: Pokemon, index: number) => (
-        <PokemonPreview key={index} pokemonURL={pokemon.url} />
+        <PokemonPreview key={index} pokemonURL={pokemon.url ?? ""} />
       ))}
     </section>
   );
